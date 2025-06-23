dd8ac1458c99bcfa20fde5fc5cb5ccd9bbec9f38
Nazar Dosmukhambetov <nazardosmukhambetov@mac.speedport.ip>
2025-06-23 23:40:14 +0200

    backend using .gitignore

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { OAuth2Client } from 'google-auth-library';
 import { getConnection } from '../config/db.js';
