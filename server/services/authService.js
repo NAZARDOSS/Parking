2757e8180ed6264c1d19df0d43e3d9a0ddd85765
Nazar Dosmukhambetov <nazardosmukhambetov@mac.speedport.ip>
2025-06-23 23:50:12 +0200

    dotenv for deploy

--- server/services/authService.js
+++ server/services/authService.js
@@ -2,8 +2,12 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { OAuth2Client } from 'google-auth-library';
 import { getConnection } from '../config/db.js';
-import dotenv from 'dotenv';
-dotenv.config();
+
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = await import('dotenv');
+  dotenv.config();
+}
+
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 console.log('Google Client ID:', process.env.GOOGLE_CLIENT_ID);
