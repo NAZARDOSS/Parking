67ee0c4ae176c28782870752b86a767133248cc6
Nazar Dosmukhambetov <nazardosmukhambetov@mac.speedport.ip>
2025-06-23 23:21:13 +0200

    adding backend to main

--- backend/server/routes/requestsRoutes.js
+++ backend/server/routes/requestsRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as reqController from '../controllers/requestsController';
+import * as reqController from '../controllers/requestsController.js';
 const router = express.Router();
 router.post('/routeInfo', reqController.addRequest);
 router.get('/getRoutes', reqController.getRequests);
